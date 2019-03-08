5e30711fecbe2a5a1d07bbe3eeb0e57fd73ff82b
Piotr Perzanowski <piotr.perzanowski@it-huset.se>
2019-03-08 18:26:52 +0100

    Test 5

--- JenkinsWithFastlineTests/JenkinsWithFastlineTests.swift
+++ JenkinsWithFastlineTests/JenkinsWithFastlineTests.swift
@@ -21,13 +21,14 @@ class JenkinsWithFastlineTests: XCTestCase {
     func testExample1() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(false, "Fail test")
+//        XCTAssert(false, "Fail test")
+        XCTAssert(true, "Pass test 1")
     }
     
     func testExample2() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(true, "Pass test")
+        XCTAssert(true, "Pass test 2")
     }
 
 //    func testPerformanceExample() {
