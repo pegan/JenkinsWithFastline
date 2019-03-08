86c171d29bc9d01e8e5196bde7f96130cd856412
Piotr Perzanowski <piotr.perzanowski@it-huset.se>
2019-03-08 13:34:15 +0100

    Initial Commit

---
+++ JenkinsWithFastline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsWithFastline
+//
+//  Created by Piotr Perzanowski on 2019-03-08.
+//  Copyright © 2019 Piotr Perzanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
