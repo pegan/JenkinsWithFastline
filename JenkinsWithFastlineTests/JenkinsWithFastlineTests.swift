89f309ba52e6cef0a6bd9c4c16c008c87a2fbef7
Piotr Perzanowski <piotr.perzanowski@it-huset.se>
2019-03-08 17:46:35 +0100

    Test 2

--- JenkinsWithFastlineTests/JenkinsWithFastlineTests.swift
+++ JenkinsWithFastlineTests/JenkinsWithFastlineTests.swift
@@ -18,17 +18,23 @@ class JenkinsWithFastlineTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testExample1() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         XCTAssert(false, "Fail test")
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testExample2() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true, "Pass test")
     }
+
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
